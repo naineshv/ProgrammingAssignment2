0e6523917f78c7772d3fe7c2d906c72985207b42
Nainesh Vashi <naineshv@gmail.com>
2014-08-21 23:41:57 -0400

    add structure of the special matrix object

--- cachematrix.R
+++ cachematrix.R
@@ -6,18 +6,28 @@
 
 ## matrixEqual: Compare matrices and return TRUE if equal, else return FALSE
 
-##Example:
-##m1 <- matrix(c(1:4),2)
-##mc <- makeCacheMatrix(m1)
-##cacheSolve(mc)
-##cacheSolve(mc)        #from cache
-##m2 <- matrix(c(5:8),2)
-##mc[["set","matrix"]](m2)
-##cacheSolve(mc)
-##cacheSolve(mc)        #from cache
-##--if the matrix hasnt changed, do not recalculate inverse
-##mc[["set","matrix"]](m2)
-##cacheSolve(mc)        #from cache
+## Example:
+## m1 <- matrix(c(1:4),2)
+## mc <- makeCacheMatrix(m1)
+## cacheSolve(mc)
+## cacheSolve(mc)        #from cache
+## m2 <- matrix(c(5:8),2)
+## mc[["set","matrix"]](m2)
+## cacheSolve(mc)
+## cacheSolve(mc)        #from cache
+## --if the matrix hasnt changed, do not recalculate inverse
+## mc[["set","matrix"]](m2)
+## cacheSolve(mc)        #from cache
+
+## makeCacheMatrix: This function creates a special "matrix" object which is a matrix of function objects.
+## These functions get and set the matrix as well as its inverse.  The returned matrix object will look as below.
+## The dimensions of the matrix have been named according to its purpose
+## makeCachematrix(m1)
+##    | matrix  inverse
+## ---|-------------------
+## set| set     setinverse
+## get| get     getinverse
+##    |
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
@@ -38,10 +48,9 @@ makeCacheMatrix <- function(x = matrix()) {
         m
 }
 
-## cacheSolve: This function checks the cache if the matrix has an inverse already calculated 
+## cacheSolve: This function checks the cache to see if the matrix has an inverse already calculated 
 ## and returns the value if available.
 ## if not available, it calculates the inverse and returns it along with caching it
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         i <- x[["get","inverse"]]()
@@ -56,7 +65,7 @@ cacheSolve <- function(x, ...) {
 }
 
 
-# matrixEqual: Compare matrices and return TRUE if equal, else return FALSE
+## matrixEqual: Compare matrices and return TRUE if equal, else return FALSE
 matrixEqual <- function(x, y) {
         (is.matrix(x) && is.matrix(y) 
         #&& dim(x) == dim(y) #if only the dims change, it does not affect the inverse, hence removing check
